868117dd07ac0c14951ccfb4d6ef9f45fd3b7389
Gustavo <gstvrrds@gmail.com>
2024-07-25 20:56:34 -0300

    feat:criando função de adicionar tarefa

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,8 +1,19 @@
+import React, { useState } from "react";
 import Button from "../Button";
 import style from "./Form.module.scss";
+
 export default function Form() {
+  const [state, setState] = useState({
+    tarefa: "",
+    tempo: "00:00",
+  });
+
+  const adicionarTarefa = (event:React.FormEvent<HTMLFormElement>)=>{
+    event.preventDefault();
+    console.log(state)
+  }
   return (
-    <form className={style.novaTarefa}>
+    <form className={style.novaTarefa} onSubmit={adicionarTarefa}>
       <div className={style.inputContainer}>
         <label htmlFor="tarefa">Adicione um novo estudo</label>
         <input
@@ -11,6 +22,8 @@ export default function Form() {
           id="tarefa"
           placeholder="O que você quer estudar"
           required
+          onChange={(e) => setState({ ...state, tarefa: e.target.value })}
+          value={state.tarefa}
         />
       </div>
       <div className={style.inputContainer}>
@@ -20,12 +33,14 @@ export default function Form() {
           step="1"
           name="tempo"
           id="tempo"
+          onChange={(e) => setState({ ...state, tempo: e.target.value })}
+          value={state.tempo}
           min="00:00:00"
           max="01:30:00"
           required
         />
       </div>
-      <Button texto = "Adicionar"/>
+      <Button texto="Adicionar" />
     </form>
   );
 }
